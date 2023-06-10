9e9bd01b038385a53baf6bdf4d60d77e41312b3e
Ravan Hashimov <ravanhashimov@gmail.com>
2023-06-11 00:07:26 +0300

    Add Home page

--- resume-management-client/src/App.tsx
+++ resume-management-client/src/App.tsx
@@ -1,18 +1,24 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./context/theme.context";
 import Navbar from "./componetns/navbar/Navbar.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home.page";
 
 function App() {
   const { darkMode } = useContext(ThemeContext);
 
   const appStyles = darkMode ? "app dark" : "app";
 
-  return <div className={appStyles}>
-    <Navbar/>
-    <div className="wrapper">
-      Routes
+  return (
+    <div className={appStyles}>
+      <Navbar />
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
---
+++ resume-management-client/src/pages/home/home.page.tsx
@@ -0,0 +1,26 @@
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="content home">
+      <h3>Welcome To Website</h3>
+      <br />
+      <br />
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad
+        animi aspernatur beatae debitis doloribus earum eveniet id in ipsa
+        laboriosam magni mollitia nisi possimus tempora, vel veritatis, vitae
+        voluptate? Accusantium ad aperiam blanditiis dicta dolorem dolores error
+        eveniet fuga illo inventore magnam molestiae nam nesciunt perferendis,
+        placeat quae, quidem quod recusandae sapiente sint? Adipisci cum dolore
+        ducimus earum magni molestiae necessitatibus odit recusandae velit
+        veniam. Aperiam assumenda, atque cum earum et eum ex fugit illo illum
+        in, iure labore laborum maiores nemo neque nisi nulla numquam quae quam
+        quas quidem quisquam ratione saepe sapiente sit tenetur totam vero
+        vitae!
+      </span>
+    </div>
+  );
+};
+
+export default Home;
