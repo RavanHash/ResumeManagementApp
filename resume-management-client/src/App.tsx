1c6234e17985adaa191d49e1280ca5317d788fb9
Ravan Hashimov <ravanhashimov@gmail.com>
2023-06-10 17:33:23 +0300

    Add ThemeContext

--- resume-management-client/src/App.tsx
+++ resume-management-client/src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ThemeContext } from "./context/theme.context";
 
 function App() {
-    return (
-        <div>App</div>
-    )
+  const { darkMode } = useContext(ThemeContext);
+
+  const appStyles = darkMode ? "app dark" : "app";
+
+  return <div>App</div>;
 }
 
-export default App
+export default App;
--- resume-management-client/src/index.tsx
+++ resume-management-client/src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./gloabal.scss";
+import ThemeContextProvider from "./context/theme.context";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ThemeContextProvider>
     <App />
-  </React.StrictMode>
+  </ThemeContextProvider>
 );
